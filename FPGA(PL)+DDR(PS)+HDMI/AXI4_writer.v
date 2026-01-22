bde57199b8079153a65a53c725638606756ace64
Sung Eunjoon <1225seong@gmail.com>
2026-01-22 22:28:12 +0900

    Update AXI4_writer.v

--- FPGA(PL)+DDR(PS)+HDMI/AXI4_writer.v
+++ FPGA(PL)+DDR(PS)+HDMI/AXI4_writer.v
@@ -10,7 +10,7 @@ module AXI4_writer(
     input wire [15:0] mixed_data, // from Chroma_key_mixer.v
     input wire pixel_valid, // from Camera_capture.v
     input wire frame_done, 
-    input wire [31:0] FRAME_BASE_ADDR,
+    input wire [31:0] FRAME_BASE_ADDR, // for Double / Triple Frame Buffer
     
     // AXI master port
     // 1. 주소 채널
@@ -27,13 +27,13 @@ module AXI4_writer(
     output wire [AXI_DATA_WIDTH -1 : 0] WDATA,
     output reg WVALID,
     input wire WREADY,
-    output reg WLAST, // 마지막
+    output reg WLAST, // 마지막 Data
     output wire [7:0] WSTRB, // Write Strobe
     
     // 3. 응답 채널
     input wire BVALID,
     output wire BREADY,
-    input wire [1:0] BRESP, // [추가] 이 줄을 꼭 넣어야 합니다!
+    input wire [1:0] BRESP, // [추가] 이거 빠져서 AXI 통신 먹통이었음.
     
     output wire o_prog_full,
     output reg [1:0] state,
@@ -44,14 +44,12 @@ module AXI4_writer(
     
     reg [1:0] next_state = 0;
     reg [7:0] data_count = 0; // 64개 세는 용도
-    
-    //reg [AXI_ADDR_WIDTH -1 : 0] ADDR_OFFSET; // 한 프레임 만들 동안 주소 300번 증가 (256 pixel * 300 = 76800 )
-    
+        
     // AXI4 Master parameter, constant
     parameter AXI_ADDR_WIDTH = 32;
     parameter AXI_DATA_WIDTH = 64;
 
-    assign AWLEN   = 8'd63;     // 64 burst
+    assign AWLEN   = 8'd79;     // 64 burst
     assign AWSIZE  = 3'b011;   // 8 byte (64 bit)
     assign AWBURST = 2'b01;    // INCR (주소 증가 모드)
     assign AWCACHE = 4'b0011; // DDR 컨트롤러 활성화 
@@ -70,6 +68,11 @@ module AXI4_writer(
     wire [63:0] fifo_data;
     wire fifo_rd_en;
     wire [8:0] rd_data_count;
+    
+    // fifo reset 신호
+    reg [3:0] reset_stretch_cnt; // 펄스를 늘리기 위한 카운터
+    wire fifo_reset_safe;        // 최종적으로 사용할 안전한 리셋 신호
+    assign fifo_reset_safe = (reset_stretch_cnt > 5);
         
     localparam IDLE = 0;
     localparam ADDR_SEND = 1;
@@ -78,10 +81,12 @@ module AXI4_writer(
     
     // frame_done을 펄스로 변환
     reg frame_done_d1;
-    wire frame_done_pulse = (frame_done == 1'b1 && frame_done_d1 == 1'b0);
+    reg frame_done_d2;
+    wire frame_done_pulse = (frame_done_d1 == 1'b1 && frame_done_d2 == 1'b0);
     
     always @(posedge clk_100Mhz) begin
         frame_done_d1 <= frame_done;
+        frame_done_d2 <= frame_done_d1;
     end
     
     // 1. sequential logic
@@ -94,12 +99,20 @@ module AXI4_writer(
             AWVALID <= 0; WVALID <= 0;
         end
         else begin
-            state <= next_state;
-            
-            if (frame_done_pulse) begin 
-                ADDR_OFFSET <= 0;   // 한 프레임 끝나면 주소 초기화
+            if (frame_done_pulse) begin
+                reset_stretch_cnt <= 8'd20; 
+                ADDR_OFFSET <= 0;
+            end
+            else if (reset_stretch_cnt > 0) begin
+                reset_stretch_cnt <= reset_stretch_cnt - 1;
+            end
+            else if (frame_done_pulse || reset_stretch_cnt > 0) begin
+                    state <= IDLE;
+                    data_count <= 0;
             end
             else begin
+                state <= next_state;
+    
                 case (state)
                     IDLE: begin
                         data_count <= 0;
@@ -121,11 +134,11 @@ module AXI4_writer(
                         if (fifo_rd_en) begin // "FWFT mode" 이므로 이 신호는 데이터 받았다는 확인 신호임. wdata는 이미 나와있는 상태.
                             data_count <= data_count + 1;
                             
-                            if (data_count == 62) begin
+                            if (data_count == 78) begin
                                 WLAST <= 1'b1;
                             end
                             
-                            if (data_count == 63) begin
+                            if (data_count == 79) begin
                                 data_count <= 0;
                                 WLAST <= 0;
                                 WVALID <= 0; // 64번 데이터 전송 (총 256픽셀)
@@ -135,13 +148,13 @@ module AXI4_writer(
                     
                     WAIT_RES: begin
                         if (BREADY && BVALID == 1) begin
-                            if (ADDR_OFFSET < 32'd153088) begin 
-                                ADDR_OFFSET <= ADDR_OFFSET + 32'd512;
+                            if (ADDR_OFFSET < 32'd152960) begin // 32'd153088 (64 burst)
+                                ADDR_OFFSET <= ADDR_OFFSET + 32'd640;//32'd512;
                             end
                         end
                     end
-                 endcase
-             end
+                endcase
+            end
         end
     end
     
@@ -162,7 +175,7 @@ module AXI4_writer(
             end
             
             DATA_SEND: begin
-                if (data_count == 63 && WREADY == 1) begin
+                if (data_count == 79 && WREADY == 1) begin
                     next_state = WAIT_RES;
                 end
             end
@@ -178,7 +191,7 @@ module AXI4_writer(
     
     // FIFO DUT
     fifo_generator_0 u_fifo_writer(
-        .rst(rst),
+        .rst(rst || fifo_reset_safe),
         .rd_data_count(rd_data_count),
         .prog_full(prog_full),
         
